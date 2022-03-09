79bd009c1f10b87fbaad6098863b31509ce93739
Theo Wyzgowski <theowyz@btinternet.com>
2022-03-09 01:28:08 +0000

    trying new approach

--- space-invaders.js
+++ space-invaders.js
@@ -5,8 +5,8 @@ const elements = {
   grid: document.querySelector('.grid'),
   width: 15,
   cells: [],
-  player: [],
-  playerLaser: [],
+  player: 215,
+  playerLaser: null,
   enemyShips: [],
   enemyLaser: [],
 }
@@ -19,16 +19,16 @@ for (let index = 0; index < elements.width ** 2; index++) {
 let movePlayer = null
 let firePlayerLaser = null
 let gameStart = false
-let moveEnemyShips = null
-let roundEnds = null
+let moveEnemyShips = 'right'
 
 //add player to board
-elements.player.push(217)
-elements.player.forEach(playericon => elements.cells[playericon].classList.add('player-image'))
+
+elements.cells[elements.player].classList.add('player-image')
 
 //add enemy ships to board
 elements.enemyShips.push(62, 63, 64, 65, 66, 67, 68, 69, 70)
 elements.enemyShips.forEach(ship => elements.cells[ship].classList.add('enemy-ship-image'))
+console.log(elements.enemyShips)
 
 //controls
 
@@ -36,10 +36,10 @@ document.addEventListener('keydown', (event) => {
   const key = event.key
 
   //command to move left
-  if (key === 'a' && !(elements.player[0] % elements.width === 0) && movePlayer !== 'right') {
+  if (key === 'a' && !(elements.player % elements.width === 0) && movePlayer !== 'right') {
     movePlayer = 'left'
     //command to move right
-  } else if (key === 'd' && !(elements.player[0] % elements.width === elements.width - 1) && movePlayer !== 'left') {
+  } else if (key === 'd' && !(elements.player % elements.width === elements.width - 1) && movePlayer !== 'left') {
     movePlayer = 'right'
     // command to fire laser
   } else if (key === 'f') {
@@ -52,11 +52,11 @@ document.addEventListener('keydown', (event) => {
   //check if enemy ships can move left, right or down
   const enemyInterval = setInterval(() => {
     for (let i = 0; i < 225; i++) {
-      if ((elements.enemyShips[0] % elements.width === 0)) {
-        moveEnemyShips = 'left'
-        console.log('the value is' + ' ' + moveEnemyShips)
-      } else if (!(elements.enemyShips[elements.enemyShips.length - 1] % elements.width === elements.width - 1)) {
+      if ((elements.enemyShips[0] % elements.width === 0 )) {
         moveEnemyShips = 'right'
+        console.log(moveEnemyShips)
+      } else if ((elements.enemyShips[elements.enemyShips.length - 1] % elements.width === elements.width - 1)) {
+        moveEnemyShips = 'left'
         console.log(moveEnemyShips)
       }
     }
@@ -66,35 +66,36 @@ document.addEventListener('keydown', (event) => {
   // enemy ships automatic movement logic
   const moveEnemiesInterval = setInterval(() => {
     //moving enemy ships right
-    if (moveEnemyShips === 'left') {
+    if (moveEnemyShips === 'right') {
       elements.enemyShips.forEach(ship => elements.cells[ship].classList.remove('enemy-ship-image'))
       elements.enemyShips.push(elements.enemyShips[8] + 1)
       elements.enemyShips.shift(elements.enemyShips[0] + 1)
       elements.enemyShips.forEach(ship => elements.cells[ship].classList.add('enemy-ship-image'))
-    } else if (moveEnemyShips === 'right') {
+      console.log(elements.enemyShips)
+    } else if (moveEnemyShips === 'left') {
       elements.enemyShips.forEach(ship => elements.cells[ship].classList.remove('enemy-ship-image'))
       elements.enemyShips.pop()
       elements.enemyShips.unshift(elements.enemyShips[0] - 1)
       elements.enemyShips.forEach(ship => elements.cells[ship].classList.add('enemy-ship-image'))
+      console.log(elements.enemyShips)
     } else if (moveEnemyShips === 'down') {
-      console.log('moving down') 
+      elements.enemyShips.forEach(ship => elements.cells[ship].classList.remove('enemy-ship-image'))
+      elements.enemyShips = elements.enemyShips + elements.width
+      elements.enemyShips.forEach(ship => elements.cells[ship].classList.add('enemy-ship-image'))
     }
   }, 1000)
 
   // move the player left and right
   if (movePlayer === 'left') {
-    elements.player.forEach(playericon => elements.cells[playericon].classList.remove('player-image'))
-    elements.player.unshift(elements.player[0] - 1)
-    elements.player.pop()
-    elements.player.forEach(playericon => elements.cells[playericon].classList.add('player-image'))
+    elements.cells [elements.player].classList.remove('player-image')
+    elements.player -= 1
+    elements.cells[elements.player].classList.add('player-image')
     movePlayer = null
     console.log(elements.player)
-
   } else if (movePlayer === 'right') {
-    elements.player.forEach(playericon => elements.cells[playericon].classList.remove('player-image'))
-    elements.player.unshift(elements.player[0] + 1)
-    elements.player.pop()
-    elements.player.forEach(playericon => elements.cells[playericon].classList.add('player-image'))
+    elements.cells[elements.player].classList.remove('player-image')
+    elements.player += 1
+    elements.cells[elements.player].classList.add('player-image')
     movePlayer = null
     console.log(elements.player[0])
   }
@@ -102,8 +103,8 @@ document.addEventListener('keydown', (event) => {
   //firing player laser  
   if (firePlayerLaser === 'fire') {
     console.log('fire command recognised')
-    elements.playerLaser.push(202)
-    elements.playerLaser.forEach(lasericon => elements.cells[lasericon].classList.add('player-laser-image'))
+    elements.playerLaser = elements.player - elements.width
+    elements.cells[elements.playerLaser].classList.add('player-laser-image')
   }
 
 })
