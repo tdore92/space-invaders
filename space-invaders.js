44cf7a6910a67da5be7f3b9ce0da73711770a1ed
Theo Wyzgowski <theowyz@btinternet.com>
2022-03-11 16:03:38 +0000

    removed object library & refactoring enemyship logic

--- space-invaders.js
+++ space-invaders.js
@@ -1,20 +1,23 @@
 
 console.log('welcome to halo space invaders')
 
-const elements = {
-  grid: document.querySelector('.grid'),
-  width: 15,
-  cells: [],
-  player: 215,
-  playerLaser: null,
-  enemyShips: [],
-  enemyLaser: [],
-}
+// my elements
 
-for (let index = 0; index < elements.width ** 2; index++) {
+const grid = document.querySelector('.grid')
+const width = 15
+let cells = []
+let player = 215
+let playerLaser = null
+let enemyShips = []
+let enemyLaser = []
+let direction = 1
+
+// creating the grid
+
+for (let index = 0; index < width ** 2; index++) {
   const div = document.createElement('div')
-  elements.grid.appendChild(div)
-  elements.cells.push(div)
+  grid.appendChild(div)
+  cells.push(div)
 }
 let movePlayer = null
 let firePlayerLaser = null
@@ -23,12 +26,12 @@ let moveEnemyShips = 'right'
 
 //add player to board
 
-elements.cells[elements.player].classList.add('player-image')
+cells[player].classList.add('player-image')
 
 //add enemy ships to board
-elements.enemyShips.push(62, 63, 64, 65, 66, 67, 68, 69, 70)
-elements.enemyShips.forEach(ship => elements.cells[ship].classList.add('enemy-ship-image'))
-console.log(elements.enemyShips)
+enemyShips.push(62, 63, 64, 65, 66, 67, 68, 69, 70)
+enemyShips.forEach(ship => cells[ship].classList.add('enemy-ship-image'))
+console.log(enemyShips)
 
 //controls
 
@@ -36,10 +39,10 @@ document.addEventListener('keydown', (event) => {
   const key = event.key
 
   //command to move left
-  if (key === 'a' && !(elements.player % elements.width === 0) && movePlayer !== 'right') {
+  if (key === 'a' && !(player % width === 0) && movePlayer !== 'right') {
     movePlayer = 'left'
     //command to move right
-  } else if (key === 'd' && !(elements.player % elements.width === elements.width - 1) && movePlayer !== 'left') {
+  } else if (key === 'd' && !(player % width === width - 1) && movePlayer !== 'left') {
     movePlayer = 'right'
     // command to fire laser
   } else if (key === 'f') {
@@ -52,77 +55,65 @@ document.addEventListener('keydown', (event) => {
   }
 
   //check if enemy ships can move left, right or down
+
+  const leftWall = (enemyShips[0] % width === 0)
+  const rightWall = (enemyShips[enemyShips.length - 1] % width === width - 1)
+
   const enemyInterval = setInterval(() => {
+    if (leftWall) {
+      direction = 1
+    } else if (rightWall) {
+      direction = -1
+    }
+    for (let i = 0; i < enemyShips.length; i++) {
+      cells[enemyShips[i]].classList.remove('enemy-ship-image')
+    }
+    for (let i = 0; i < enemyShips.length; i++) {
+      enemyShips[i] += direction
+    }
+    for (let i = 0; i < enemyShips.length; i++) {
+      cells[enemyShips[i]].classList.add('enemy-ship-image')
+    }
+    
     for (let i = 0; i < 225; i++) {
-      if ((elements.enemyShips[0] % elements.width === 0)) {
-        moveEnemyShips = 'right'
-        console.log(moveEnemyShips)
-      } else if ((elements.enemyShips[elements.enemyShips.length - 1] % elements.width === elements.width - 1)) {
-        moveEnemyShips = 'left'
-        console.log(moveEnemyShips)
+      if (leftWall && direction === - 1 || rightWall && direction === 1) {
+        direction = width
       }
     }
   }, 500)
 
-  function moveShipsDown() {
-    elements.enemyShips.forEach(ship => elements.cells[ship].classList.remove('enemy-ship-image'))
-    elements.enemyShips = elements.enemyShips.map(ship => ship + elements.width)
-    elements.enemyShips.forEach(ship => elements.cells[ship].classList.add('enemy-ship-image'))
-  }
-
-
   // enemy ships automatic movement logic
-  const moveEnemiesInterval = setInterval(() => {
-    //moving enemy ships right
-    if (moveEnemyShips === 'right') {
-      elements.enemyShips.forEach(ship => elements.cells[ship].classList.remove('enemy-ship-image'))
-      elements.enemyShips.push(elements.enemyShips[8] + 1)
-      elements.enemyShips.shift(elements.enemyShips[0] + 1)
-      elements.enemyShips.forEach(ship => elements.cells[ship].classList.add('enemy-ship-image'))
-      console.log(elements.enemyShips)
-    } else if (moveEnemyShips === 'left') {
-      elements.enemyShips.forEach(ship => elements.cells[ship].classList.remove('enemy-ship-image'))
-      elements.enemyShips.pop()
-      elements.enemyShips.unshift(elements.enemyShips[0] - 1)
-      elements.enemyShips.forEach(ship => elements.cells[ship].classList.add('enemy-ship-image'))
-      console.log(elements.enemyShips)
-    }
-  }, 500)
+
 
   // move the player left and right
   if (movePlayer === 'left') {
-    elements.cells[elements.player].classList.remove('player-image')
-    elements.player -= 1
-    elements.cells[elements.player].classList.add('player-image')
+    cells[player].classList.remove('player-image')
+    player -= 1
+    cells[player].classList.add('player-image')
     movePlayer = null
-    console.log(elements.player)
+    console.log(player)
   } else if (movePlayer === 'right') {
-    elements.cells[elements.player].classList.remove('player-image')
-    elements.player += 1
-    elements.cells[elements.player].classList.add('player-image')
+    cells[player].classList.remove('player-image')
+    player += 1
+    cells[player].classList.add('player-image')
     movePlayer = null
-    console.log(elements.player[0])
+    console.log(player[0])
   }
 
   //firing player laser  
   if (firePlayerLaser === 'fire') {
     console.log('fire command recognised')
-    elements.playerLaser = elements.player - elements.width
-    elements.cells[elements.playerLaser].classList.add('player-laser-image')
+    playerLaser = player - width
+    cells[playerLaser].classList.add('player-laser-image')
     laserFiring()
   }
 
   function laserFiring() {
     const laserFiringInterval = setInterval(() => {
       if (firePlayerLaser === 'fire') {
-        elements.cells[elements.playerLaser].classList.remove('player-laser-image')
-        elements.playerLaser = elements.playerLaser - elements.width
-        elements.cells[elements.playerLaser].classList.add('player-laser-image')
-        for (let i = 0; i < 225; i++) {
-          if (elements.cells[elements.enemyShips] === elements.cells[elements.playerLaser]) {
-            console.log('thats a hit!')
-          }
-        }
+        cells[playerLaser].classList.remove('player-laser-image')
+        playerLaser = playerLaser - width
+        cells[playerLaser].classList.add('player-laser-image')
       }
     }, 100)
   }
