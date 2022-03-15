74ac3ef303c6c7ef401cecdc2c7e36bf682da87b
Theo Wyzgowski <theowyz@btinternet.com>
2022-03-15 15:47:07 +0000

    enemy and player laser logic

--- space-invaders.js
+++ space-invaders.js
@@ -6,7 +6,7 @@ console.log('welcome to halo space invaders')
 const grid = document.querySelector('.grid')
 const width = 15
 let cells = []
-let player = 215
+let player = []
 let playerLaser = null
 let enemyShips = []
 let enemyLaser = []
@@ -19,19 +19,20 @@ for (let index = 0; index < width ** 2; index++) {
   grid.appendChild(div)
   cells.push(div)
 }
+
 let movePlayer = null
 let firePlayerLaser = null
 let gameStart = false
-let moveEnemyShips = 'right'
 
 //add player to board
 
+player.push(215)
 cells[player].classList.add('player-image')
 
 //add enemy ships to board
+
 enemyShips.push(62, 63, 64, 65, 66, 67, 68, 69, 70)
 enemyShips.forEach(ship => cells[ship].classList.add('enemy-ship-image'))
-console.log(enemyShips)
 
 //controls
 
@@ -56,8 +57,6 @@ document.addEventListener('keydown', (event) => {
 
   //check if enemy ships can move left, right or down
 
-
-
   const enemyInterval = setInterval(() => {
     const leftWall = (enemyShips[0] % width === 0)
     const rightWall = (enemyShips[enemyShips.length - 1] % width === width - 1)
@@ -69,7 +68,7 @@ document.addEventListener('keydown', (event) => {
       console.log('direction is ' + direction)
     }
 
-    if (leftWall && direction ===  - 1 || rightWall && direction === 1) {
+    if (leftWall && direction === - 1 || rightWall && direction === 1) {
       direction = width
     }
     for (let i = 0; i < enemyShips.length; i++) {
@@ -80,13 +79,14 @@ document.addEventListener('keydown', (event) => {
     }
     for (let i = 0; i < enemyShips.length; i++) {
       cells[enemyShips[i]].classList.add('enemy-ship-image')
-    }   
+    }
   }, 500)
 
   // enemy ships automatic movement logic
 
 
   // move the player left and right
+
   if (movePlayer === 'left') {
     cells[player].classList.remove('player-image')
     player -= 1
@@ -101,7 +101,8 @@ document.addEventListener('keydown', (event) => {
     console.log(player[0])
   }
 
-  //firing player laser  
+  //firing player laser
+
   if (firePlayerLaser === 'fire') {
     console.log('fire command recognised')
     playerLaser = player - width
@@ -116,9 +117,43 @@ document.addEventListener('keydown', (event) => {
         playerLaser = playerLaser - width
         cells[playerLaser].classList.add('player-laser-image')
       }
-    }, 100)
+      for (let i = 0; i < enemyShips.length; i++) {
+        if (cells[playerLaser] === cells[enemyShips[i]]) {
+          enemyShips.splice(i, 1)
+          clearInterval(laserFiringInterval)
+          cells[playerLaser].classList.remove('player-laser-image')
+        }
+      }
+    }, 200)
   }
 
+  // firing enemy lasers
+
+  //randomly select enemy ship to fire laser
+
+  const enemyAttackSelectionInterval = setInterval(() => {
+    enemyLaser = enemyShips[Math.floor(enemyShips.length * Math.random())]
+    enemyLaser = enemyLaser + width
+    cells[enemyLaser].classList.add('player-laser-image')
+    enemyLaserFiring()
+  }, 2500)
+
+  // enemy laser firing
+
+  function enemyLaserFiring() {
+    const enemyLaserFiringInterval = setInterval(() => {
+      cells[enemyLaser].classList.remove('player-laser-image')
+      enemyLaser = enemyLaser + width
+      cells[enemyLaser].classList.add('player-laser-image')
+      for (let i = 0; i < player.length; i++) {
+        if (cells[enemyLaser] === cells[player]) {
+          console.log('player dead!')
+        }
+      }
+    }, 200)
+
+
+  }
 })
 
 
